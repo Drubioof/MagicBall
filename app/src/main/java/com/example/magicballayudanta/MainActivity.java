36696ec787e1b5acd09ec4ed2cdd27ff93887458
Drubioof <daniel.rubio@mail.udp.cl>
2020-05-10 21:29:19 -0400

    Retoques visuales, comentarios agregados

--- app/src/main/java/com/example/magicballayudanta/MainActivity.java
+++ app/src/main/java/com/example/magicballayudanta/MainActivity.java
@@ -12,19 +12,20 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     //Variables
-    private int timesAppIsResumed = -1;
+    private int timesAppIsResumed = -1;     //Contador para mostrar mensajes cuando la app es resumida y no al inicio
     private ImageView ball;
-    private TextView respuesta, textAboveBall, changeUsage;
+    private TextView respuesta, textAboveBall, changeUsage; //textAboveBall es el texto arriba de la bola ; changeUsage es el texto en la parte inferior de la app utilizado como botón
     private String[] respuestasPosibles={"Es cierto", "Definitivamente es así", "Sin duda", "Sí definitivamente", "Puede confiar en él", "Como yo lo veo, sí",
             "Lo más probable", "Perspectiva buena", "Sí", "Las señales apuntan a sí", "Respuesta confusa intente de nuevo", "Pregunte de nuevo más tarde",
             "Mejor no decirte ahora", "No puedo predecir ahora", "Concéntrate y pregunta de nuevo", "No cuentes con eso",
             "Mi respuesta es no", "Mis fuentes dicen que no", "Outlook no es tan bueno", "Muy dudoso"};
-    private String[] respuestasPosibles1 = {"1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.9",
+    private String[] respuestasPosibles1 = {"1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.9", //Otras respuestas para cuando se cambia de modalidad
             "2.0", "2.1", "2.2", "2.3", "2.4", "2.5", "2.6", "2.7", "2.8", "2.9", "3.0", "3.1", "3.2", "3.3",
             "3.4", "3.5", "3.6", "3.7", "3.8", "3.9", "4.0", "4.1", "4.2", "4.3", "4.4", "4.5", "4.6", "4.7",
             "4.8", "4.9", "5.0", "5.1", "5.2", "5.3", "5.4", "5.5", "5.6", "5.7", "5.8", "5.9", "6.0", "6.1",
             "6.2", "6.3", "6.4", "6.5", "6.6", "6.7", "6.8", "6.9", "7.0"};
-    private String mensaje[] = {"Sabía que volverías!", "Tu destino era volver :)", "La bola nos dijo que volverías!", "Todos vuelven :)"};
+    private String mensaje[] = {"Sabía que volverías!", "Tu destino era volver :)", "La bola nos dijo que volverías!", "Todos vuelven :)"}; //Posibles mensajes al resumir la app
+
     //Métodos
     //Al abrir la app por primera vez
     @Override
@@ -44,24 +45,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
-    //Al resumir la app
+    //Para mostrar mensajes al resumir la app
     @Override
     protected void onResume(){
         super.onResume();
+
         timesAppIsResumed += 1;
-        int aleatorio = new Random().nextInt(mensaje.length);
+
         if(timesAppIsResumed >= 1){
+            int aleatorio = new Random().nextInt(mensaje.length);
             Toast.makeText(this, mensaje[aleatorio], Toast.LENGTH_SHORT).show();
         }
+
     }
 
-    //Al hacer click en imagen
+    //Al hacer click en imagen o en el texto para cambiar función
     @Override
     public void onClick(View v){
-        if(v.getId() == R.id.boton){
+
+        if(v.getId() == R.id.boton){ //Imagen
+
             int numRandom = new Random().nextInt(respuestasPosibles.length);
             respuesta.setText(respuestasPosibles[numRandom]);
-        }else if(v.getId() == R.id.notas){
+
+        }else if(v.getId() == R.id.notas){ //Texto
+
             String aux = (String) changeUsage.getText();
             String[] aux1 = respuestasPosibles;
 
@@ -72,5 +80,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             respuestasPosibles1 = aux1;
 
         }
+
     }
+
 }
